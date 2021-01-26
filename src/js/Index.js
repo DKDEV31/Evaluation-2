8451c055a567ff3fddd7ddd82ba513f73e7f9934
Benjamin Faucheux <benjaminfaucheux40@gmail.com>
2021-01-26 19:46:32 +0100

    JS side dice feature
    without rolling

--- src/js/Index.js
+++ src/js/Index.js
@@ -11,11 +11,19 @@ const round1 = $('#current1')
 const round2 = $('#current2')
 const player1 = $('#player1')
 const player2 = $('#player2')
+const one = $('.one')
+const two = $('.two')
+const three = $('.three')
+const four = $('.four')
+const five = $('.five')
+const six = $('.six')
+const seven = $('.seven')
 //Variables
 let round = 0
 let globalPlayer1 = 12
 let globalPlayer2 = 0
 let playerInGame = 0
+let diceRender = 0
 //Instructions
 
 newGame.click(() => {
@@ -45,10 +53,13 @@ newGame.hover(
 )
 // Functions
 function rollAgain() {
+    diceRoll(0)
     let randomDiceShot = Math.floor(Math.random() * 6)
     console.log(randomDiceShot)
     if (randomDiceShot !== 1) {
         round += randomDiceShot
+        diceRender = randomDiceShot
+        diceRoll(diceRender)
         if (playerInGame === 1) {
             round1.text(round)
         } else {
@@ -56,6 +67,7 @@ function rollAgain() {
         }
     } else {
         round = 0
+        diceRoll(1)
         if (playerInGame === 1) {
             playerInGame = 2
             player1.toggleClass('active-player')
@@ -95,7 +107,59 @@ function playAgain() {
     globalPlayer1 = 0
     globalPlayer2 = 0
     playerInGame = 1
+    player2.removeClass('active-player')
+    player1.removeClass('active-player')
     player1.addClass('active-player')
     round1.text(0)
     round2.text(0)
+    diceRoll(0)
+}
+
+function diceRoll(diceNumber) {
+    switch (diceNumber) {
+        case 0:
+            one.css('opacity', 0)
+            two.css('opacity', 0)
+            three.css('opacity', 0)
+            four.css('opacity', 0)
+            five.css('opacity', 0)
+            six.css('opacity', 0)
+            seven.css('opacity', 0)
+            break
+        case 1:
+            seven.css('opacity', 1)
+            break
+        case 2:
+            two.css('opacity', 1)
+            three.css('opacity', 1)
+            break
+        case 3:
+            two.css('opacity', 1)
+            three.css('opacity', 1)
+            seven.css('opacity', 1)
+            break
+        case 4:
+            two.css('opacity', 1)
+            three.css('opacity', 1)
+            one.css('opacity', 1)
+            four.css('opacity', 1)
+            break
+        case 5:
+            one.css('opacity', 1)
+            two.css('opacity', 1)
+            three.css('opacity', 1)
+            four.css('opacity', 1)
+            seven.css('opacity', 1)
+            break
+        case 6:
+            five.css('opacity', 1)
+            six.css('opacity', 1)
+            one.css('opacity', 1)
+            four.css('opacity', 1)
+            two.css('opacity', 1)
+            three.css('opacity', 1)
+            break
+        default:
+            break
+    }
 }
